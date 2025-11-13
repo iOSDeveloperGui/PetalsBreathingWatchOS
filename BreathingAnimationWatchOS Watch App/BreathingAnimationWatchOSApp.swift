e4fd6be468346fc5400e2394dd5bdd63d3a6a586
iOSDeveloperGui <guilhermesilvadias535@gmail.com>
2025-11-13 15:06:08 -0300

    Initial Commit

---
+++ BreathingAnimationWatchOS Watch App/BreathingAnimationWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathingAnimationWatchOSApp.swift
+//  BreathingAnimationWatchOS Watch App
+//
+//  Created by iOS Developer on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BreathingAnimationWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
